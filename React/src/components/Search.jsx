afbce0dfb8ca4dac9183cc698e419c4ace5c692a
N3rson <124893280+N3rson@users.noreply.github.com>
2024-08-02 12:39:47 +0200

    delete

--- React/src/components/Search.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-
-function Search(props) {
-
-    return (
-        <input type="text" value={props.search} onChange={props.handleSearch} />
-    )
-}
-export default Search
